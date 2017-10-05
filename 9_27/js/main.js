575af13a8e5776a825f5e4af0afa40602482eff6
Drew C <drewgamesaw@gmail.com>
2017-10-04 21:34:08 -0400

    loading stuff

--- 9_27/js/main.js
+++ 9_27/js/main.js
@@ -3,3 +3,9 @@ function load() {
     document.getElementById("content").style.display = "block";
 }
 
+
+// When the page is fully loaded, it calls the load() function, which hides the loading icon and shows the rest of the content
+document.onreadystatechange = function () {
+
+    if (document.readyState == 'complete') {load();}
+}
