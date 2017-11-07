220e40b0d39b725d68cfbc402bfe76e91d5a6190
Drew C <drew.ciaramitaro51@gmail.com>
2017-11-07 12:59:49 -0500

    xd

---
+++ DailyNotes/10_30/js/js.js
@@ -0,0 +1,40 @@
+$(document).on('scroll', function (e) {
+    if ($(document).scrollTop() > 1)
+    {
+        $('.navbar').addClass("scrolled");
+    }
+    else
+    {
+        $('.navbar').removeClass("scrolled");
+    }
+});
+
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
